ece4ad9ee8e92fc267d2a240a191f6524df413bc
Himanshusinghofficial <hs882677@gmail.com>
2020-07-02 02:19:52 +0530

    Updates

---
+++ precache-manifest.4a1f115af59a12702cc1d15088f220b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8f6637fed0f3cd68a9d0265f16dbd4",
+    "url": "/ITLogger/index.html"
+  },
+  {
+    "revision": "46448ac50a9e9b8f2ba3",
+    "url": "/ITLogger/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "9b175bd42f19b7b7daa1",
+    "url": "/ITLogger/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "46448ac50a9e9b8f2ba3",
+    "url": "/ITLogger/static/js/2.fadd1756.chunk.js"
+  },
+  {
+    "revision": "9b175bd42f19b7b7daa1",
+    "url": "/ITLogger/static/js/main.27a51918.chunk.js"
+  },
+  {
+    "revision": "b586720000437f562b9c",
+    "url": "/ITLogger/static/js/runtime~main.7225604a.js"
+  }
+]);
